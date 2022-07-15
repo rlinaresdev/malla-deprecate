7f3b16f2f5d6cf8576d0d155e9a9e716a9d277fc
Ramon A Linares <rlinares4381@gmail.com>
2022-07-15 14:32:10 -0400

    Projecto Malla

---
+++ src/System/Core/Driver.php
@@ -0,0 +1,43 @@
+<?php
+namespace Malla\Core;
+
+/*
+ *---------------------------------------------------------
+ * ©IIPEC
+ * Santo Domingo República Dominicana.
+ *---------------------------------------------------------
+*/
+
+class Driver {
+
+   public function info() {
+  		return [
+  			"name"			=> "Core",
+  			"author"		   => "Ing. Ramón A Linares Febles",
+  			"email"			=> "rlinares4381@gmail.com",
+  			"license"		=> "MIT",
+  			"support"		=> "http://www.iipec.net",
+  			"version"		=> "V-1.0",
+  			"description" 	=> "Core V-1.0",
+  		];
+  	}
+
+  	public function app() {
+  		return [
+  			"type"			=> "core",
+  			"slug"			=> "core",
+  			"driver"			=> \Malla\Core\Driver::class,
+  			"token"			=> NULL,
+  			"activated" 	=> 1,
+  		];
+  	}
+
+  	public function meta() {
+  		return [
+  		];
+  	}
+
+  	public function handler($core) {
+  		$core->create($this->app())->addInfo($this->info())->addMeta("type", $this->meta());
+  	}
+}
